b7748131de7f89ad237e601ad93b79f296e26f5f
ext_herbert.wirnsber <herbert.wirnsberger@gepardec.com>
2019-01-31 05:00:56 +0100

    add dependencie and create java class with import

---
+++ aws-lamda/src/main/java/at/test/ImportAWSLamda.java
@@ -0,0 +1,8 @@
+package at.test;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class ImportAWSLamda {}
